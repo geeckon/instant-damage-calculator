8d971fbc27ae18d04886e499faffad5b080ca3e8
YvesW <yves_w@ymail.com>
2023-02-06 22:18:32 +0100

    Formatting

--- src/main/java/com/geeckon/instantdamagecalculator/InstantDamageCalculatorPlugin.java
+++ src/main/java/com/geeckon/instantdamagecalculator/InstantDamageCalculatorPlugin.java
@@ -85,7 +85,131 @@ public class InstantDamageCalculatorPlugin extends Plugin
 		put(NPCWithXpBoost.GENERAL_GRAARDOR, 1.325).
 		put(NPCWithXpBoost.KRIL_TSUTSAROTH, 1.375).
 		put(NPCWithXpBoost.CORPOREAL_BEAST, 1.55).
+		put(NPCWithXpBoost.BANDIT_130, 1.025).
+		put(NPCWithXpBoost.BLACK_DRAGON, 1.05).
+		put(NPCWithXpBoost.BRUTAL_BLACK_DRAGON, 1.1).
+		put(NPCWithXpBoost.DARK_WARRIOR_135,1.1).
+		put(NPCWithXpBoost.ENT_WILDERNESS,1.025).
+		put(NPCWithXpBoost.ENT_WC_GUILD,1.05).
+		put(NPCWithXpBoost.FIRE_GIANT_104_109,1.025).
+		put(NPCWithXpBoost.BRUTAL_GREEN_DRAGON, 1.05).
+		put(NPCWithXpBoost.LAVA_DRAGON, 1.075).
+		put(NPCWithXpBoost.MOSS_GIANT_84, 1.025).
+		put(NPCWithXpBoost.GREATER_NECHRYAEL, 1.025).
+		put(NPCWithXpBoost.REVENANT_HOBGOBLIN, 1.025).
+		put(NPCWithXpBoost.REVENANT_CYCLOPS, 1.075).
+		put(NPCWithXpBoost.REVENANT_HELLHOUND, 1.075).
+		put(NPCWithXpBoost.REVENANT_DEMON, 1.075).
+		put(NPCWithXpBoost.REVENANT_ORK, 1.1).
+		put(NPCWithXpBoost.REVENANT_DARK_BEAST, 1.125).
+		put(NPCWithXpBoost.REVENANT_KNIGHT, 1.175).
+		put(NPCWithXpBoost.REVENANT_DRAGON, 1.2).
+		put(NPCWithXpBoost.REVENANT_MALEDICTUS, 1.65).
+		put(NPCWithXpBoost.SPIRITUAL_WARRIOR_BANDOS, 1.025).
+		put(NPCWithXpBoost.DEVIANT_SPECTRE, 1.025).
+		put(NPCWithXpBoost.ADAMANT_DRAGON, 1.1).
+		put(NPCWithXpBoost.DEMONIC_GORILLA, 1.075).
+		put(NPCWithXpBoost.TORTURED_GORILLA, 1.125).
+		put(NPCWithXpBoost.BLUE_DRAGON, 1.025).
+		put(NPCWithXpBoost.BRUTAL_BLUE_DRAGON, 1.05).
+		put(NPCWithXpBoost.DARK_BEAST, 1.025).
+		put(NPCWithXpBoost.DRAKE, 1.075).
+		put(NPCWithXpBoost.ELVES, 1.025).
+		put(NPCWithXpBoost.SPITTING_WYVERN, 1.025).
+		put(NPCWithXpBoost.TALONED_WYVERN, 1.025).
+		put(NPCWithXpBoost.LONGTAILED_WYVERN, 1.025).
+		put(NPCWithXpBoost.ANCIENT_WYVERN, 1.05).
+		put(NPCWithXpBoost.IRON_DRAGON, 1.05).
+		put(NPCWithXpBoost.LIZARDMAN_SHAMAN_OVERWORLD, 1.05).
+		put(NPCWithXpBoost.MITHRIL_DRAGON, 1.075).
+		put(NPCWithXpBoost.RED_DRAGON, 1.025).
+		put(NPCWithXpBoost.BRUTAL_RED_DRAGON, 1.075).
+		put(NPCWithXpBoost.RUNE_DRAGON, 1.1).
+		put(NPCWithXpBoost.SKELETAL_WYVERN, 1.05).
+		put(NPCWithXpBoost.STEEL_DRAGON, 1.05).
+		put(NPCWithXpBoost.SUQAH, 1.025).
+		put(NPCWithXpBoost.BRONZE_DRAGON, 1.025).
+		put(NPCWithXpBoost.ICE_TROLLS, 1.05).
+		put(NPCWithXpBoost.TROLL_GENERAL, 1.075).
+		put(NPCWithXpBoost.WYRM, 1.025).
+		put(NPCWithXpBoost.LOCUST_RIDER, 1.025).
+		put(NPCWithXpBoost.SCARAB_MAGE, 1.025).
+		put(NPCWithXpBoost.HYDRA, 1.075).
+		put(NPCWithXpBoost.REPUGNANT_SPECTRE, 1.075).
+		put(NPCWithXpBoost.BASILISK_SENTINEL, 1.075).
+		put(NPCWithXpBoost.KING_KURASK, 1.025).
+		put(NPCWithXpBoost.MARBLE_GARGOYLE, 1.025).
+		put(NPCWithXpBoost.NECHRYACH, 1.025).
+		put(NPCWithXpBoost.GREATER_ABYSSAL_DEMON, 1.05).
+		put(NPCWithXpBoost.NIGHT_BEAST, 1.175).
+		put(NPCWithXpBoost.CORRUPT_LIZARDMAN_NMZ, 1.025).
+		put(NPCWithXpBoost.ELVARG_NMZ, 1.025).
+		put(NPCWithXpBoost.MOSS_GUARDIAN_NMZ, 1.025).
+		put(NPCWithXpBoost.MOSS_GUARDIAN_HARD_NMZ, 1.075).
+		put(NPCWithXpBoost.SLAGILITH_NMZ, 1.03).
+		put(NPCWithXpBoost.SLAGILITH_HARD_NMZ, 1.025).
+		put(NPCWithXpBoost.DAGANNOTH_MOTHER_NMZ, 1.05).
+		put(NPCWithXpBoost.DAGANNOTH_MOTHER_HARD_NMZ, 1.125).
+		put(NPCWithXpBoost.DAD_NMZ, 1.05).
+		put(NPCWithXpBoost.DAD_HARD_NMZ, 1.1).
+		put(NPCWithXpBoost.ARRG_NMZ, 1.075).
+		put(NPCWithXpBoost.ARRG_HARD_NMZ, 1.175).
+		put(NPCWithXpBoost.BLACK_KNIGHT_TITAN_NMZ, 1.025).
+		put(NPCWithXpBoost.BLACK_KNIGHT_TITAN_HARD_NMZ, 1.05).
+		put(NPCWithXpBoost.ICE_TROLL_KING_NMZ, 1.075).
+		put(NPCWithXpBoost.ICE_TROLL_KING_HARD_NMZ, 1.125).
+		put(NPCWithXpBoost.GLOD_NMZ, 1.05).
+		put(NPCWithXpBoost.GLOD_HARD_NMZ, 1.1).
+		put(NPCWithXpBoost.EVIL_CHICKEN_NMZ, 0.025).
+		put(NPCWithXpBoost.AGRITHNANA_NMZ, 1.125).
+		put(NPCWithXpBoost.AGRITHNANA_HARD_NMZ, 1.175).
+		put(NPCWithXpBoost.FLAMBEED_NMZ, 1.125).
+		put(NPCWithXpBoost.FLAMBEED_HARD_NMZ, 1.175).
+		put(NPCWithXpBoost.KARAMEL_NMZ, 1.05).
+		put(NPCWithXpBoost.DESSOURT_NMZ, 1.1).
+		put(NPCWithXpBoost.DESSOURT_HARD_NMZ, 1.2).
+		put(NPCWithXpBoost.GELATINNOTH_MOTHER_NMZ, 1.075).
+		put(NPCWithXpBoost.GELATINNOTH_MOTHER_HARD_NMZ, 1.125).
+		put(NPCWithXpBoost.CHRONOZON_NMZ, 0.1).
+		put(NPCWithXpBoost.DESSOUS_NMZ, 1.1).
+		put(NPCWithXpBoost.DESSOUS_HARD_NMZ, 1.175).
+		put(NPCWithXpBoost.DAMIS_FIRST_NMZ, 1.05).
+		put(NPCWithXpBoost.DAMIS_SECOND_NMZ, 1.15).
+		put(NPCWithXpBoost.DAMIS_FIRST_HARD_NMZ, 1.1).
+		put(NPCWithXpBoost.DAMIS_SECOND_HARD_NMZ, 1.225).
+		put(NPCWithXpBoost.FAREED_NMZ, 1.15).
+		put(NPCWithXpBoost.FAREED_HARD_NMZ, 1.25).
+		put(NPCWithXpBoost.KAMIL_NMZ, 1.125).
+		put(NPCWithXpBoost.KAMIL_HARD_NMZ, 1.225).
+		put(NPCWithXpBoost.BARRELCHEST_NMZ, 1.1).
+		put(NPCWithXpBoost.BARRELCHEST_HARD_NMZ, 1.175).
+		put(NPCWithXpBoost.GIANT_SCARAB_NMZ, 1.05).
+		put(NPCWithXpBoost.GIANT_SCARAB_HARD_NMZ, 1.1).
+		put(NPCWithXpBoost.JUNGLE_DEMON_NMZ, 1.075).
+		put(NPCWithXpBoost.JUNGLE_DEMON_HARD_NMZ, 1.15).
+		put(NPCWithXpBoost.ARIANWYN_NMZ, 1.025).
+		put(NPCWithXpBoost.ESSYLLT_NMZ, 1.05).
+		put(NPCWithXpBoost.ESSYLLT_HARD_NMZ, 1.1).
+		put(NPCWithXpBoost.DAGANNOTH_WATERBIRTH_MELEE, 1.05).
+		put(NPCWithXpBoost.GIANT_ROCK_CRAB, 1.1).
+		put(NPCWithXpBoost.WALLASALKI, 1.025).
+		put(NPCWithXpBoost.ROCK_LOBSTER, 1.025).
+		put(NPCWithXpBoost.JUSTICIAR_ZACHARIAH, 1.55).
+		put(NPCWithXpBoost.DERWEN, 1.2).
+		put(NPCWithXpBoost.PORAZDIR, 1.2).
+		put(NPCWithXpBoost.GALVEK, 1.425).
+		put(NPCWithXpBoost.FRAGMENT_OF_SEREN, 1.55).
+		put(NPCWithXpBoost.GLOUGH, 1.325).
+		put(NPCWithXpBoost.MONKEY_GUARD, 1.025).
+		put(NPCWithXpBoost.MONKEY_SKELETON, 1.025).
+		put(NPCWithXpBoost.DOUBLE_AGENT_108, 1.025).
+		put(NPCWithXpBoost.DOUBLE_AGENT_141, 1.05).
+		put(NPCWithXpBoost.SARADOMIN_WIZARD, 1.025).
+		put(NPCWithXpBoost.ANCIENT_WIZARD_MELEE, 1.05).
+		put(NPCWithXpBoost.UNDEAD_DRUID, 1.05).
+		put(NPCWithXpBoost.FEROCIOUS_BARBARIAN_SPIRIT, 1.05).
 		build();
+	
 	private static final ImmutableMap<NPCWithXpBoost, Double[]> XP_MODIFIERS_WITH_MODES = ImmutableMap.<NPCWithXpBoost, Double[]>builder().
 		put(NPCWithXpBoost.TEKTON, new Double[] {1.35, 1.5}).
 		put(NPCWithXpBoost.TEKTON_ENRAGED, new Double[] {1.525, 1.775}).
@@ -99,6 +223,12 @@ public class InstantDamageCalculatorPlugin extends Plugin
 		put(NPCWithXpBoost.SKELETAL_MYSTIC, new Double[] {1.2, 1.3}).
 		put(NPCWithXpBoost.MUTTADILE_SMALL, new Double[] {1.125, 1.225}).
 		put(NPCWithXpBoost.MUTTADILE_LARGE, new Double[] {1.2, 1.35}).
+		put(NPCWithXpBoost.PESTILENT_BLOAT, new Double[] {1.85, 1.975, 1.075}).
+		put(NPCWithXpBoost.NYLOCAS_VASILIAS, new Double[] {1.225, 1.225, 1.025}).
+		put(NPCWithXpBoost.SOTETSEG, new Double[] {1.675, 1.675, 1.045}).
+		put(NPCWithXpBoost.VERZIK_VITUR_P1, new Double[] {1.05, 1.05, 1.005}).
+		put(NPCWithXpBoost.VERZIK_VITUR_P2, new Double[] {1.425, 1.425, 1.025}).
+		put(NPCWithXpBoost.VERZIK_VITUR_P3, new Double[] {1.85, 1.85, 1.125}).
 		build();
 
 	private HashMap<Integer, Double> CUSTOM_XP_MODIFIERS = new HashMap<Integer, Double>();
